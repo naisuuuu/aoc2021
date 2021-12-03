380966d94a47f4f2a43d5a2ddea91975f4fed81d
naisuuuu <47899783+naisuuuu@users.noreply.github.com>
2021-12-03 14:15:28 +0100

    Day 3 - less lines, more bit twiddling

--- day3/main.go
+++ day3/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"strconv"
-	"strings"
 
 	"github.com/naisuuuu/aoc2021/input"
 )
@@ -20,91 +19,51 @@ func main() {
 
 func p1(in []string) {
 	ones := make([]int, len(in[0]))
-	zeroes := make([]int, len(in[0]))
 	for _, i := range in {
 		for j, c := range i {
-			if c == '0' {
-				zeroes[j]++
-			} else {
+			if c == '1' {
 				ones[j]++
 			}
 		}
 	}
-	var gamma, eps []string
-	for i := range ones {
-		if ones[i] > zeroes[i] {
-			gamma = append(gamma, "1")
-			eps = append(eps, "0")
-		} else {
-			gamma = append(gamma, "0")
-			eps = append(eps, "1")
+	var gamma uint
+	for i, cnt := range ones {
+		if cnt > len(in)/2 {
+			gamma += 1 << (len(ones) - 1 - i)
 		}
 	}
-	gi, _ := strconv.ParseInt(strings.Join(gamma, ""), 2, 64)
-	ei, _ := strconv.ParseInt(strings.Join(eps, ""), 2, 64)
-	fmt.Println("Part 1:", gi*ei)
+	fmt.Println("Part 1:", gamma*(^gamma&(1<<len(ones)-1)))
+}
+
+func sieve(in []string, a, b byte) int64 {
+	options, matches := in, []string{}
+	for i := 0; i < len(in[0]); i++ {
+		if len(options) <= 1 {
+			break
+		}
+		var zeroes int
+		for _, o := range options {
+			if o[i] == '0' {
+				zeroes++
+			}
+		}
+		f := a
+		if zeroes > len(options)/2 {
+			f = b
+		}
+		for _, o := range options {
+			if o[i] == f {
+				matches = append(matches, o)
+			}
+		}
+		options, matches = matches, []string{}
+	}
+	out, _ := strconv.ParseInt(options[0], 2, 64)
+	return out
 }
 
 func p2(in []string) {
-	options := in
-	matches := make([]string, 0)
-	for i := 0; i < len(in[0]); i++ {
-		if len(options) <= 1 {
-			break
-		}
-		var ones, zeroes int
-		for _, o := range options {
-			if o[i] == '0' {
-				zeroes++
-			} else {
-				ones++
-			}
-		}
-		gv := '1'
-		if zeroes > ones {
-			gv = '0'
-		}
-		for _, o := range options {
-			if rune(o[i]) == gv {
-				matches = append(matches, o)
-			}
-		}
-		options = matches
-		matches = make([]string, 0)
-	}
-
-	generator := options[0]
-
-	options = in
-	matches = make([]string, 0)
-	for i := 0; i < len(in[0]); i++ {
-		if len(options) <= 1 {
-			break
-		}
-		var ones, zeroes int
-		for _, o := range options {
-			if o[i] == '0' {
-				zeroes++
-			} else {
-				ones++
-			}
-		}
-		gv := '0'
-		if zeroes > ones {
-			gv = '1'
-		}
-		for _, o := range options {
-			if rune(o[i]) == gv {
-				matches = append(matches, o)
-			}
-		}
-		options = matches
-		matches = make([]string, 0)
-	}
-
-	scrubber := options[0]
-
-	gi, _ := strconv.ParseInt(generator, 2, 64)
-	si, _ := strconv.ParseInt(scrubber, 2, 64)
-	fmt.Println("Part 2:", gi*si)
+	g := sieve(in, '1', '0')
+	s := sieve(in, '0', '1')
+	fmt.Println("Part 2:", g*s)
 }
