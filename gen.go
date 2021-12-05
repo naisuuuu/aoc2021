74a8cca8bb3b3545c9878c674d03894d41c6fe35
naisuuuu <47899783+naisuuuu@users.noreply.github.com>
2021-12-05 04:49:34 +0100

    Automate input and example download

--- gen.go
+++ gen.go
@@ -4,62 +4,161 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-const template = `package main
+const (
+	aocURL   = "https://adventofcode.com/%d/day/%d"
+	template = `package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/naisuuuu/aoc2021/input"
 )
 
 func main() {
-	in, err := input.Read()
+	in, err := input.Read("example")
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := solve(in); err != nil {
-		log.Fatal(err)
-	}
+	solve(in)
 }
 
-func solve(in []string) error {
-	return nil
+func solve(in []string) {
+	fmt.Println("Part 1:", in)
 }
 `
+)
 
 func main() {
 	day := flag.Int("d", 1, "day of AOC")
+	year := flag.Int("y", 2021, "year of AOC")
 	flag.Parse()
-	dir := fmt.Sprintf("day%d", *day)
-	_, err := os.Stat(dir)
+
+	if err := gen(*day, *year); err != nil {
+		log.Fatalln("Error", err)
+	}
+}
+
+func gen(day, year int) error {
+	dir := fmt.Sprintf("day%d", day)
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("creating dir: %w", err)
+	}
+
+	if err := writeTemplate(filepath.Join(dir, "main.go")); err != nil {
+		return fmt.Errorf("writing template: %w", err)
+	}
+
+	if err := downloadExample(filepath.Join(dir, "example"), year, day); err != nil {
+		log.Printf("Error downloading example: %v", err)
+	}
+
+	cookie, err := os.ReadFile(".cookie")
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			log.Println(".cookie not found. Input will not be downloaded.")
+			return nil
+		}
+		return fmt.Errorf("reading .cookie: %v", err)
+	}
+
+	if err := downloadInput(
+		strings.Trim(string(cookie), " \n"),
+		filepath.Join(dir, "input"),
+		year, day); err != nil {
+		return fmt.Errorf("downloading input: %v", err)
+	}
+
+	return nil
+}
+
+func downloadInput(cookie, fpath string, year, day int) error {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(aocURL, year, day)+"/input", nil)
+	if err != nil {
+		return fmt.Errorf("preparing request: %w", err)
+	}
+	req.AddCookie(&http.Cookie{Name: "session", Value: cookie})
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("executing request: %w", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Printf("input download non-200 status code: %d", res.StatusCode)
+	}
+
+	f, err := os.Create(fpath)
+	if err != nil {
+		return fmt.Errorf("creating file: %w", err)
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, res.Body); err != nil {
+		return fmt.Errorf("copying body: %w", err)
+	}
+
+	return nil
+}
+
+func downloadExample(fpath string, year, day int) error {
+	res, err := http.Get(fmt.Sprintf(aocURL, year, day))
+	if err != nil {
+		return fmt.Errorf("getting puzzle body: %w", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return fmt.Errorf("non-200 status code: %d", res.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return fmt.Errorf("parsing puzzle body: %w", err)
+	}
+
+	example := doc.FindMatcher(goquery.Single(".day-desc pre code")).Text()
+	if len(strings.Trim(example, " \n")) == 0 {
+		return fmt.Errorf("zero length example: '%s'", example)
+	}
+
+	f, err := os.Create(fpath)
+	if err != nil {
+		return fmt.Errorf("creating file: %w", err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(example); err != nil {
+		return fmt.Errorf("writing example: %w", err)
+	}
+
+	return nil
+}
+
+func writeTemplate(fpath string) error {
+	_, err := os.Stat(fpath)
 	if err == nil {
-		log.Fatalf("%s already exists", dir)
+		log.Printf("%s already exists. Template will not be written", fpath)
+		return nil
 	}
 	if !errors.Is(err, fs.ErrNotExist) {
-		log.Fatalf("Error stating %s: %v", dir, err)
+		return fmt.Errorf("stat %s: %w", fpath, err)
 	}
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-		log.Fatalf("Error creating dir: %v", err)
+
+	if err := os.WriteFile(fpath, []byte(template), 0644); err != nil {
+		return fmt.Errorf("creating main.go: %w", err)
 	}
-	if err := os.WriteFile(filepath.Join(dir, "main.go"), []byte(template), 0644); err != nil {
-		log.Fatalf("Error creating main.go: %v", err)
-	}
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("Error getting home dir: %v", err)
-	}
-	dl := filepath.Join(home, "Downloads", "input")
-	if _, err := os.Stat(dl); err != nil {
-		log.Println("No input file in downloads. Returning early")
-		return
-	}
-	if err := os.Rename(dl, filepath.Join(dir, "input")); err != nil {
-		log.Fatalf("Error moving input from downloads: %v", err)
-	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/naisuuuu/aoc2021
 
 go 1.17
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
