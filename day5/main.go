d3cf0b3b69a18175410af77f29888245416c4a99
naisuuuu <47899783+naisuuuu@users.noreply.github.com>
2021-12-05 07:31:09 +0100

    Day 5

---
+++ day5/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/naisuuuu/aoc2021/conv"
+	"github.com/naisuuuu/aoc2021/input"
+)
+
+func main() {
+	// in, err := input.Read("input")
+	in, err := input.Read("example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	solve(in)
+}
+
+type coord [4]int
+
+func newCoord(s string) coord {
+	cs := strings.Split(s, " -> ")
+	a := conv.AtoiS(cs[0], ",")
+	b := conv.AtoiS(cs[1], ",")
+	return coord{a[0], a[1], b[0], b[1]}
+}
+
+type grid [][]int
+
+func newGrid(side int) grid {
+	grid := make([][]int, side+1)
+	for i := range grid {
+		grid[i] = make([]int, side+1)
+	}
+	return grid
+}
+
+func (g grid) cnt() int {
+	var cnt int
+	for row := range g {
+		for col := range g[0] {
+			if g[row][col] > 1 {
+				cnt += 1
+			}
+		}
+	}
+	return cnt
+}
+
+func solve(in []string) {
+	var side int
+	var coords []coord
+	for _, l := range in {
+		c := newCoord(l)
+		coords = append(coords, c)
+		for _, co := range c {
+			if co > side {
+				side = co
+			}
+		}
+	}
+
+	g := newGrid(side)
+
+	for _, c := range coords {
+		if c[0] != c[2] && c[1] != c[3] {
+			// Ignore diagonal lines for p1.
+			continue
+		}
+
+		minx, maxx := c[0], c[2]
+		if c[0] > c[2] {
+			minx, maxx = c[2], c[0]
+		}
+
+		miny, maxy := c[1], c[3]
+		if c[1] > c[3] {
+			miny, maxy = c[3], c[1]
+		}
+
+		for x := minx; x <= maxx; x++ {
+			for y := miny; y <= maxy; y++ {
+				g[y][x]++
+			}
+		}
+	}
+
+	fmt.Println("Part 1:", g.cnt())
+
+	for _, c := range coords {
+		if c[0] == c[2] || c[1] == c[3] {
+			// Straight lines have already been applied.
+			continue
+		}
+
+		xinc := 1
+		if c[0] > c[2] {
+			xinc = -1
+		}
+
+		yinc := 1
+		if c[1] > c[3] {
+			yinc = -1
+		}
+
+		x, y := c[0], c[1]
+		for {
+			g[y][x]++
+			if x == c[2] && y == c[3] {
+				break
+			}
+			x += xinc
+			y += yinc
+		}
+		continue
+	}
+
+	fmt.Println("Part 2:", g.cnt())
+}
